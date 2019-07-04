f79b83efbd8a3a881a0281fe32df1abc7b3b6c92
Ville-Pekka Lahti <ville-pekka.lahti@kotivo.fi>
2019-07-04 09:47:20 +0300

    jea it wörks

--- src/server.c
+++ src/server.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/socket.h>
@@ -41,20 +42,61 @@ struct users *last_user;
 
 int recv_socket = -1;
 
+int server_fd, new_socket, valread, max_sd;
+struct sockaddr_in address;
+int opt;
+int addrlen;
+char buffer[1024];
+fd_set readfds;
+
 void adduser(char *nick, int socket)
 {
-	struct users *new_user = malloc(sizeof(struct users));
-	strcpy(new_user->nick, nick);
-	new_user->socket = socket;
-	new_user->prev = last_user;
-	last_user->next = new_user;
-	last_user = new_user;
-
+	max_sd = max_sd > socket ? max_sd : socket;
+	if (strcmp(last_user->nick, "") == 0) {
+		strcpy(last_user->nick, nick);
+		last_user->socket = socket;
+	} else {
+		struct users *new_user = malloc(sizeof(struct users));
+		strcpy(new_user->nick, nick);
+		new_user->socket = socket;
+		new_user->prev = last_user;
+		last_user->next = new_user;
+		last_user = new_user;
+	}
 }
 
 void removeuser(char *nick)
 {
+	struct users *cur_u;
+	cur_u = all_users;
+	while (cur_u->nick) {
+		if (strcmp(cur_u->nick, nick) == 0) {
+			if (cur_u->prev != NULL) {
+				cur_u->prev->next = cur_u->next;
+			}
+			if (cur_u->next != NULL) {
+				cur_u->next->prev = cur_u->prev;
+			}
+			struct users *todel = cur_u;
+			cur_u = cur_u->next;
+			free(todel);
+			return;
+		}
+		cur_u = cur_u->next;
+	}
+}
 
+int getsocketbynick(char *nick)
+{
+	struct users *cur_u;
+	cur_u = all_users;
+	while (cur_u->nick) {
+		if (strcmp(cur_u->nick, nick) == 0) {
+			return cur_u->socket;
+		}
+		cur_u = cur_u->next;
+	}
+	return 0;
 }
 
 void listusers()
@@ -63,10 +105,24 @@ void listusers()
 	cur_u = all_users;
 	printf("Listing all users:\n");
 	while (cur_u->nick) {
-		printf("- %s\n", cur_u->nick);
+		printf("- <%s>\n", cur_u->nick);
 		cur_u = cur_u->next;
 	}
 	printf("________________\n");
+}
+
+char *listusers_string(char users_string[])
+{
+	strcpy(users_string, "");
+	struct users *cur_u;
+	cur_u = all_users;
+	while (cur_u->nick) {
+		strcat(users_string, "[");
+		strcat(users_string, cur_u->nick);
+		strcat(users_string, "] ");
+		cur_u = cur_u->next;
+	}
+	return users_string;
 }
 
 void init_users()
@@ -80,18 +136,13 @@ void clear_users()
 
 }
 
-int server_fd, new_socket, valread, max_sd;
-struct sockaddr_in address;
-int opt;
-int addrlen;
-char buffer[1024];
-fd_set readfds;
+
 
 void init_connection(int port)
 {
 
 	opt = 1;
-	addrlen = sizeof(address);
+	
 	memset(&buffer, 0, sizeof(buffer));
 
 	// Creating socket file descriptor
@@ -100,7 +151,7 @@ void init_connection(int port)
 		exit(EXIT_FAILURE);
 	}
 
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR,
 	               &opt, sizeof(opt))) {
 		perror("setsockopt");
 		exit(EXIT_FAILURE);
@@ -118,55 +169,116 @@ void init_connection(int port)
 		perror("listen");
 		exit(EXIT_FAILURE);
 	}
-	max_sd = server_fd;
-
-	// Clear an fd_set
-	FD_ZERO(&readfds);
+	addrlen = sizeof(address);
+	
 
 	// Add a descriptor to an fd_set
-	FD_SET(server_fd, &readfds);
+	
 
 }
 
+void send_everyone_but(char *nick, char *send_string)
+{
+	struct users *cur_u;
+	cur_u = all_users;
+	while (cur_u->nick) {
+		if (strcmp(cur_u->nick, nick) != 0) {
+			send(cur_u->socket ,  send_string, strlen(send_string) , 0);
+		}
+		cur_u = cur_u->next;
+	}
+}
+
+
 void receive_sync(char ** received_msg)
 {
+	FD_ZERO(&readfds);
+	FD_SET(server_fd, &readfds);
+	max_sd = server_fd;
+
+	struct users *cur_u;
+	cur_u = all_users;
+	while (cur_u->nick) {
+		FD_SET(cur_u->socket, &readfds);
+		max_sd = max_sd > cur_u->socket ? max_sd : cur_u->socket;
+		cur_u = cur_u->next;
+	}
+
+	char * welcome = "Welcome to the server, \033[1m%s\033[0m!\nUsers in the server: %s\n";
+	char *welcomenick;
+	char *invalid_nick = "\033[01;33mMessage from the server: \033[1;31mERROR! Please reconnect with \033[0m\033[1m/nick <nick>\033[0m\n";
+
+	printf("waiting for activity...\n");
 	int activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);
+	printf("some activity received!\n");
 
 	if ((activity < 0) && (errno != EINTR)) {
 		printf("select error");
 	}
 
-	char *hello = "Hello from server";
+	if (activity == 0) {
+		return;
+	}
+
+	printf("checking master sock\n");
 	if (FD_ISSET(server_fd, &readfds)) {
+		printf("something in master sock, accepting incoming sock...\n");
 		if ((new_socket = accept(server_fd,
 		                         (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
 			perror("accept");
 			exit(EXIT_FAILURE);
 		}
+		printf("new sock from master sock accepted, reading shit...\n");
 		valread = read( new_socket , buffer, 1024);
-		char *delim = " ";
-		char *word = strtok(buffer, delim);
+		printf("read ok\n");
+		char *space = " ";
+		char *linebreak = "\n";
+		char *word = strtok(buffer, space);
 		if (strcmp(word, "/nick") == 0) {
-			word = strtok(NULL, delim);
+			word = strtok(NULL, space);
+			word = strtok(word, linebreak);
 			adduser(word, new_socket);
-
-			printf("nick set: %s\nsocket: %d\n", word, new_socket);
+			printf("\033[1;32mNick \033[0m\033[1m%s\033[0m \033[1;32mconnected. Socket: \033[0m\033[1m%d\033[0m\n", word, new_socket);
+			char u_str[4096];
+			listusers_string(u_str);
+			asprintf(&welcomenick, welcome, word, u_str);
+			send(new_socket ,  welcomenick, strlen(welcomenick) , 0);
 		} else {
-			printf("paskaa\n");
+			printf("\033[01;33mSomeone tried to connect without telling nick. Warned them about it.\n\033[0m");
+			send(new_socket ,  invalid_nick, strlen(invalid_nick) , 0);
 		}
-		printf("%s\n", buffer );
-		send(new_socket , hello , strlen(hello) , 0 );
-		printf("Hello message sent\n");
-		listusers();
 		return;
-	}
+	} else {
+		printf("existing sock...\n");
+		struct users *cur_u;
+		cur_u = all_users;
+		while (cur_u->nick) {
+			if (FD_ISSET(cur_u->socket, &readfds)) {
+				printf("reading...\n");
+				if ((valread = read(cur_u->socket, buffer, 1024)) == 0) {
+					printf("%s disconnected!\n", cur_u->nick);
+					close(cur_u->socket);
+					removeuser(cur_u->nick);
+				} else {
+					char *linebreak = "\n";
+					char *msg = strtok(buffer, linebreak);
 
+					printf("MSG from %s: %s\n", cur_u->nick, msg);
+					send_everyone_but(cur_u->nick, msg);
+					return;
+				}
+
+			}
+			cur_u = cur_u->next;
+		}
+		printf("existing sock operations completed\n");
+	}
 
 }
 
 int main()
 {
-	int port = 6667;
+	int port = 1234;
 
 	init_users();
 	init_connection(port);
